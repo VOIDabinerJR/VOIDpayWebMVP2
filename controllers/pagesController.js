d0a0a859d0deae5f4659ab15e473c15c60961d11
Abiner Maleiane Jr <abinermaleianejr@gmail.com>
2024-07-27 17:37:46 +0200

    profileupdate

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -5,12 +5,34 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 
-exports.singup_get = async (req, res) => {
-    return res.status(400).send("Passwords do not match");
+exports.profile = async (req, res) => {  
+    const { firstName, lastName, username, email, password, repeatPassword,dateOfBirth,address, postalCode, documentId, documentIdImg,phone,alternativeEmail,businessName, legalDocument, form }  = req.body;
+    const token = req.cookies.jwt;
+    
+    try {
+        const response = await fetch('http://localhost:3000/pages/profile', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ token, firstName, lastName, username, email, password, repeatPassword,dateOfBirth,address, postalCode, documentId, documentIdImg,phone,alternativeEmail,businessName, legalDocument, form } )
+        });
 
+        const data = await response.json();
+        console.log(data);
+
+        if (response.status === 201) {
+            const maxAge = 3 * 24 * 60 * 60 * 1000; // Exemplo de tempo de expiração do cookie
+
+            // Define o cookie JWT no cliente
+            res.cookie('jwt', data.token, { httpOnly: true, maxAge });
+
+            // Redireciona para o dashboard
+            return res.redirect('/dashboard');
+        } else {
+            // Trata qualquer erro retornando o status e mensagem recebidos do servidor
+            return res.status(response.status).send(data);
+        }
+    } catch (error) {
+        console.error('Erro durante o registro:', error);
+        return res.status(500).send('Erro interno do servidor');
+    }
 };
-exports.login_get = async (req, res) => {
-    return res.status(400).send("Passwords do not match");
-
-};
-
--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const authController = require('../controllers/pagesController');
+const pagesController = require('../controllers/pagesController');
+
 const router = express.Router();
 const { requireAuth } = require('../middleware/authMiddleware');
 const { loadUserData } = require('../middleware/dataLoadMiddleware');
@@ -9,17 +10,16 @@ const { loadUserData } = require('../middleware/dataLoadMiddleware');
 
 
 
-
+//main
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../','public/Mainpage/index.html'));
 });
 
 
-
+//auth
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'login.html'));
 });
-
 router.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'register.html'));
 });
@@ -30,6 +30,9 @@ router.get('/recoveraccount', (req, res) => {
 router.get('/resetpassword', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'reset-password.html'));
 });
+
+
+//other
 router.get('/privacy', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'privacy.html'));
 });
@@ -41,6 +44,17 @@ router.get('/legal', (req, res) => {
 
 
 
+//user
+router.get('/profile', requireAuth, loadUserData,(req, res) => {
+    const data = req.data
+
+    res.sendFile(path.join(__dirname, '../', 'profile.html'));
+    //.render('profile', {user: data.user, userDetails:data.userDetails, businessDetails: data.businessDetails})
+   
+});
+router.post('/profile', pagesController.profile );
+
+
 
 router.get('/dashboard', requireAuth, loadUserData,(req, res) => {
     const data = req.data
@@ -48,6 +62,7 @@ router.get('/dashboard', requireAuth, loadUserData,(req, res) => {
     res.render('index', {token: data.firstname})
    
 });
+
 
 router.get('/integracaoSimples', requireAuth,loadUserData, (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'integracaoSimples.html'));
